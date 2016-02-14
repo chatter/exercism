95a1bbcb3610c9a2056ddc58729daff242c56682
Curtis Hatter <mitchell.hatter@gmail.com>
2016-02-14 17:54:20 -0500

    refactor: moved cond statements to functions
    also removed unnecessary match

--- elixir/bob/bob.exs
+++ elixir/bob/bob.exs
@@ -1,11 +1,16 @@
 defmodule Bob do
   def hey(input) do
     cond do
-      String.match?(input, ~r/^\p{Z}*$/) -> "Fine. Be that way!"
-      String.match?(input, ~r/^(?=.*\p{Lu})[\P{Ll}]+!?[^?]$/) -> "Whoa, chill out!"
-      String.last(input) == "!" -> "Whatever."
-      String.last(input) == "?" -> "Sure."
+      silent?(input) -> "Fine. Be that way!"
+      yelling?(input) -> "Whoa, chill out!"
+      sure?(input) -> "Sure."
       true -> "Whatever."
     end
   end
+
+  defp silent?(input), do: String.match?(input, ~r/^\p{Z}*$/)
+  defp yelling?(input), do: String.match?(input, ~r/^(?=.*\p{Lu})[\P{Ll}]+!?[^?]$/)
+  defp whatever?(input), do: String.last(input) == "!" 
+  defp sure?(input), do: String.last(input) == "?"
 end
+
