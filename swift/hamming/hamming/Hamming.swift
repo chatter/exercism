0e02746745b051908da4c9c03be8028af2f026bc
Curtis Hatter <mitchell.hatter@gmail.com>
2016-02-14 23:42:01 -0500

    refactor: use guard instead of if

--- swift/hamming/hamming/Hamming.swift
+++ swift/hamming/hamming/Hamming.swift
@@ -1,7 +1,7 @@
 struct Hamming {
     static func compute(origin: String, against: String) -> Int? {
         // return quick if strings aren't the same size
-        if origin.characters.count != against.characters.count { return nil }
+        guard origin.characters.count == against.characters.count else { return nil }
 
         return zip(origin.characters, against.characters).reduce(0) { (acc: Int, tuple: (a: Character, b: Character)) in
             return acc + (tuple.a == tuple.b ? 0 : 1)
