54b77396ab4158aed5092b011906e95900e4530f
Curtis Hatter <mitchell.hatter@gmail.com>
2016-02-15 23:26:58 -0500

    feat: acronym implementation
    all tests pass

--- elixir/acronym/acronym.exs
+++ elixir/acronym/acronym.exs
@@ -5,6 +5,10 @@ defmodule Acronym do
   """
   @spec abbreviate(string) :: String.t()
   def abbreviate(string) do
+    string
+      |> String.split(~r/[\p{Z}\p{P}]|(?=[A-Z])/, trim: true)
+      |> Enum.reduce("", &(&2 <> String.first(&1)))
+      |> String.upcase
    
   end
 end
