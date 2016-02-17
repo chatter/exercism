ee0f823770de45948c52be08202ae8d7af728b23
Curtis Hatter <mitchell.hatter@gmail.com>
2016-02-16 22:42:54 -0800

    feat: sublist compare finished
    all tests pass, not too happy with the implementation and feel like i'll be revisiting this one later.

--- elixir/sublist/sublist.exs
+++ elixir/sublist/sublist.exs
@@ -4,6 +4,23 @@ defmodule Sublist do
   and if not whether it is equal or unequal to the second list.
   """
   def compare(a, b) do
+    cond do
+      a == b -> :equal
+      Enum.count(a) == 0 -> :sublist
+      Enum.count(b) == 0 -> :superlist
+      do_subset?(a, b) -> :sublist
+      do_subset?(b, a) -> :superlist
+      true -> :unequal
+    end
+  end
 
+  defp do_subset?(a, []), do: false
+  defp do_subset?(a, [head | tail] = b) do
+    count_a = Enum.count(a)
+    cond do
+      Enum.count(b) < count_a -> false
+      a === Enum.take(b, count_a) -> true
+      true -> do_subset?(a, tail)
+    end
   end
 end
