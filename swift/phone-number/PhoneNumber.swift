77948d11d43083efe7165d1f595be5998c8646e0
Curtis Hatter <mitchell.hatter@gmail.com>
2016-02-21 19:31:13 -0500

    feat: impelemented phone number formatting

---
+++ swift/phone-number/PhoneNumber.swift
@@ -0,0 +1,41 @@
+//
+//  PhoneNumber.swift
+//  phone-number
+//
+//  Created by Mitchell Hatter on 2/21/16.
+//  Copyright © 2016 Mitchell Hatter. All rights reserved.
+//
+
+struct PhoneNumber : CustomStringConvertible {
+  let number: String
+
+  var areaCode: String {
+    return number[number.startIndex...number.startIndex.advancedBy(2)]
+  }
+
+  var description: String {
+    let co = self.number[number.startIndex.advancedBy(3)...number.startIndex.advancedBy(5)]
+    let te = self.number[number.startIndex.advancedBy(6)...number.startIndex.advancedBy(9)]
+
+    return "(\(self.areaCode)) \(co)-\(te)"
+  }
+
+  init(_ number: String) {
+    self.number = PhoneNumber.format(number)
+  }
+
+  private static func format(number: String) -> String {
+    var c = number.characters.filter {
+      return !["(", ")", "-", " ", "."].contains($0)
+    }
+
+    guard c.count >= 10 else { return "0000000000" }
+
+    if c.count > 10 {
+      guard c.first == "1" else { return "0000000000" }
+      c.removeFirst()
+    }
+
+    return String(c)
+  }
+}
--- swift/phone-number/README.md
+++ swift/phone-number/README.md
@@ -5,14 +5,14 @@ Write a program that cleans up user-entered phone numbers so that they can be se
 The rules are as follows:
 
 - If the phone number is less than 10 digits assume that it is bad
-  number
+number
 - If the phone number is 10 digits assume that it is good
 - If the phone number is 11 digits and the first number is 1, trim the 1
-  and use the last 10 digits
+and use the last 10 digits
 - If the phone number is 11 digits and the first number is not 1, then
-  it is a bad number
+it is a bad number
 - If the phone number is more than 11 digits assume that it is a bad
-  number
+number
 
 We've provided tests, now make them pass.
 
