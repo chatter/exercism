272178dc0d10f3a5951240be3416864781a80f94
Curtis Hatter <mitchell.hatter@gmail.com>
2016-03-12 16:45:48 -0500

    refactor: replace reduce with count

--- elixir/hamming/hamming.exs
+++ elixir/hamming/hamming.exs
@@ -10,7 +10,7 @@ defmodule DNA do
   @spec hamming_distance([char], [char]) :: non_neg_integer
   def hamming_distance(strand1, strand2) when length(strand1) == length(strand2) do
     Enum.zip(strand1, strand2)
-    |> Enum.reduce(0, fn({x, y}, acc) -> if x != y, do: acc + 1, else: acc end)
+    |> Enum.count(fn({x, y}) -> x != y end)
   end
 
   def hamming_distance(strand1, strand2), do: nil
